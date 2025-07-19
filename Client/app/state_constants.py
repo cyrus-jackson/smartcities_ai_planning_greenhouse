dfb1bb33c6babf9851c73ac233a2223983574b49
Cyrus Jackson <cyrusjackson@gmail.com>
2025-07-19 14:55:10 +0200

    trying to fix client

--- Client/app/state_constants.py
+++ Client/app/state_constants.py
@@ -1,4 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+if repo_root not in sys.path:
+    sys.path.insert(0, repo_root)
 from shared.state_constants import *
